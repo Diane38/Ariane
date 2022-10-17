7011f5214ccdc8a90de186774c2e12c6a7786579
Diane38 <113247605+Diane38@users.noreply.github.com>
2022-10-17 14:16:18 +0200

    Add files via upload

---
+++ Arianeuh/src/StratPourWin.java
@@ -0,0 +1,38 @@
+
+public class StratPourWin {
+
+	/**
+	 * Pour la suite, je vous propose une maquette pour la classe Strategie. Vous
+	 * pouvez la modifier comme bon vous semble.
+	 */
+	
+	/**
+	 * le robot se déplace et balaye autour de lui pour chercher les palets.
+	 */
+	private void detection() {
+		
+	}
+	
+	/**
+	 * le robot veut capturer un palet. 
+	 * @param 
+	 */
+	private void attraper() {}
+	
+	/**
+	 *  le robot capture un palet. 
+	 * @param args
+	 */
+	private void saisir() {}
+	
+	/**
+	 * le robot fait une pause et se réinitialise
+	 */
+	private void reinitialize() {}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Arianeuh/src/Ultrason.java
@@ -0,0 +1,15 @@
+
+public class Ultrason {
+	public boolean estMur() {
+		return true;
+	}
+
+	public boolean estRobot() {
+		return true;
+	}
+
+	public boolean estPalet() {
+		return true;
+	}
+
+}
