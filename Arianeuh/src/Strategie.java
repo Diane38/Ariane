65436293b92086850dd52eb3e1401e2ac71c82fe
Diane38 <113247605+Diane38@users.noreply.github.com>
2022-10-24 13:05:47 +0200

    Add files via upload

---
+++ Arianeuh/src/Strategie.java
@@ -0,0 +1,25 @@
+
+public class Strategie {
+
+	// penser aux threads : pour faire plusieurs stratégies --> faire des threads
+	// (autres programmes qui fonctionnent en parallèle
+
+	private void rechercherPalet() {
+
+	}
+
+	private void attraperPalet() {
+
+	}
+
+	private void jaiPalet() {
+	}
+
+	private boolean estPalet() {
+		return false;
+	}
+
+	private boolean estMur() {
+		return false;
+	}
+}
